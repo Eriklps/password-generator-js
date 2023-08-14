87deec69917969716b792416325f944de690b073
Eriklps <erikdslopes@gmail.com>
2023-08-14 14:27:21 -0300

    lazy load test

--- js/scripts.js
+++ js/scripts.js
@@ -105,3 +105,23 @@ copyPasswordButton.addEventListener("click", (e) => {
   });
 });
 
+// lazy load 
+
+const images = document.querySelectorAll("#register-banner");
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    const image = entry.target;
+
+    image.src = image.src.replace("&w=10&", "&w=1000&");
+
+    observer.unobserve(image);
+  });
+}, {});
+
+images.forEach((image) => {
+  observer.observe(image);
+});
+
